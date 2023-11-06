677a33f0d6a55cb7ec609904c52037b1e70f72bf
Nicolas Fuentes <nfuentes11@gmail.com>
2023-11-06 17:08:31 -0500

    Basic logic structure implemented

--- script.js
+++ script.js
@@ -5,8 +5,38 @@ const gameBoard = (function () {
   for (let i = 0; i < rows; i++) {
     board[i] = [];
     for (let j = 0; j < columns; j++) {
-      board[i].push([j]);
+      board[i][j] = [];
     }
   }
+  const getBoard = () => board;
+
   console.table(board);
 })();
+
+function player(name, marker) {
+  return {
+    name,
+    marker,
+    score: 0,
+    addScore: function () {
+      this.score++;
+    },
+  };
+}
+
+const displayController = (function () {})();
+
+const game = {
+  playeOne: player("Palyer One", "X"),
+  PlayerTwo: player("Player Two", "O"),
+  winPattern: [
+    [[0][0], [0][1], [0][2]],
+    [[1][0], [1][1], [1][2]],
+    [[2][0], [2][1], [2][2]],
+    [[0][0], [1][0], [2][0]],
+    [[0][1], [1][1], [2][1]],
+    [[0][2], [1][2], [2][2]],
+    [[2][0], [1][1], [0][2]],
+    [[0][0], [1][1], [2][2]],
+  ],
+};
